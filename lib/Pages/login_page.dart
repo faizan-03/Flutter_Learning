f8932fd85e6d7f6f631e72d162c0f1018b158ba6
Rana Faizan Ali <ranafaizaan03@gmail.com>
2025-06-03 03:47:10 +0500

    Day4 Completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -18,17 +18,68 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            TextField(decoration: InputDecoration(labelText: 'Email')),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
+            Image.asset(
+              'assets/images/Alumnii_logo.png',
+              fit: BoxFit.cover,
+              height: 200,
+              width: 200,
             ),
-
-            ElevatedButton(
-              onPressed: () {
-                // Handle login logic
-              },
-              child: Text('Login'),
+            SizedBox(height: 20),
+            Text(
+              'WELCOME TO ALUMNII',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      prefixIcon: Icon(Icons.email),
+                      hintText: 'Enter your email',
+                    ),
+                  ),
+                  SizedBox(height: 20), // Add some space between fields
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  SizedBox(height: 20), // Add some space between fields
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 15,
+                      ),
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {
+                      print('Login button pressed');
+                    },
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/home_page.dart';
 import 'package:flutter_application_1/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light, // Set the theme mode to dark
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.lato().fontFamily, // Use Lato font for the app
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+        // Custom title text style
+      ),
+      // Use Google Fonts for the app
       darkTheme: ThemeData.dark(), // Use the default dark theme
       debugShowCheckedModeBanner: false, // Hide the debug banner
 
